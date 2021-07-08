196313c5c6ced8d4620ef647da9c985580ab0082
koji koyama <k.koyama37@gmail.com>
2021-07-08 23:10:59 +0900

    :recycle: yarn eslint --fixによる修正

--- __tests__/index.test.tsx
+++ __tests__/index.test.tsx
@@ -5,6 +5,6 @@ import Home from '../pages'
 
 it('Should render hello text', () => {
   render(<Home />)
-  const heading = screen.getByText(/Next.js features/i);
-  expect(heading).toBeInTheDocument();
+  const heading = screen.getByText(/Next.js features/i)
+  expect(heading).toBeInTheDocument()
 })
